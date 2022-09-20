67f97d94c60decc32ca568f651ab1cf2d86d45ee
ahmed elamin <ahmedselamin@gmail.com>
2022-09-20 08:47:26 +0200

    done

--- whatsapp.py
+++ whatsapp.py
@@ -3,12 +3,12 @@ import pywhatkit
 
 #for contacts
 
-#phone_no = input("Enter phone number: ")
+phone_no = input("Enter phone number: ")
 
-#pywhatkit.sendwhatmsg(phone_no,"the message", 8,33, 1, True, 2)
+pywhatkit.sendwhatmsg(phone_no,"the message", 8,33, 1, True, 2)
 
 #for group
 
-group_id = input("Enter groupe id: ")
+#group_id = input("Enter groupe id: ")
 
-pywhatkit.sendwhatmsg_to_group(group_id,"Tessssstttt",8, 41)
+#pywhatkit.sendwhatmsg_to_group(group_id,"msg",hr, min)
